c97cc33b9f7d0437ce13d889a1b96cb8a94429e5
Alex Forsythe <forsythe@hackedu.io>
2021-07-02 16:44:59 -0500

    Add variety to the message for count > 100 (breaks a unit test)

--- src/app.py
+++ src/app.py
@@ -13,7 +13,9 @@ def get_message(count):
         return "Sure as snuff, there ain't no digits in that there string!"
     if count > 100:
         estimate = (count // 100) * 100
-        return f"Tarnation! That's a whole heap of digits! There must be at least {estimate} numbers in there!"
+        interjection = 'Consarnit' if count % 2 == 0 else 'Tarnation'
+        noun = 'heap' if count // 5 == 0 else 'mess'
+        return f"{interjection}! That's a whole {noun} of digits! There must be at least {estimate} numbers in there!"
     return f"I reckon I see {count} digits."
 
 
