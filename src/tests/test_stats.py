5e26858aa6ede19f0d8ae353dfb9b5c9a5d77779
Alex Forsythe <forsythe@hackedu.io>
2021-07-06 16:25:11 -0500

    Fix stats test in CI

--- src/tests/test_stats.py
+++ src/tests/test_stats.py
@@ -1,5 +1,8 @@
+from __version__ import version
+
+
 def test_stats(client):
     r = client.get("/stats")
     assert r.status_code == 200
     assert int(r.json["num"]) > 0
-    assert r.json["version"] == "0"
+    assert r.json["version"] == version
